fd450e16b406e2a02ef6e2d6e9be88af22dfc347
hai57 <hoanghaiick28@gmail.com>
2024-03-19 12:59:15 +0700

    update

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,11 +1,9 @@
 import { authenticateToken, generateToken, checkTokenValidity } from './authenticateToken.js'
 import upload from "./multer.js";
-import timeoutMiddleware from './timeoutMiddleware.js'
 
 export {
   generateToken,
   upload,
   authenticateToken,
-  checkTokenValidity,
-  timeoutMiddleware
+  checkTokenValidity
 }
